7b07e40974b3366c89119acbe7f51187b5a0de9f
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-18 00:15:30 +0000

    Automatically backed up by Learn

--- db/migrate/20200318000918_create_posts.rb
+++ db/migrate/20200318000918_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.stirng :title
+      t.string :title
       t.text :description
 
       t.timestamps
